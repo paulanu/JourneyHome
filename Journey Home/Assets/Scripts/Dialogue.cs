a2fcc00c0007ddc18e911dab542328808a807dd8
Paula <pnavarr1@ucsc.edu>
2019-01-26 11:16:08 -0800

    dialogue errors fixed

--- Journey Home/Assets/Scripts/Dialogue.cs
+++ Journey Home/Assets/Scripts/Dialogue.cs
@@ -5,24 +5,24 @@ using System.IO;
 
 public class Dialogue : MonoBehaviour
 {
-	protected FileInfo dialogueText = null;
-    protected StreamReader reader;
-    protected string text = " "; // assigned to allow first line to be read below
+	private FileInfo dialogueText = null;
+    private StreamReader reader;
+    private string line = " "; // assigned to allow first line to be read below
 
     // Start is called before the first frame update
     void Start()
     {
-        dialogueText = new FileInfo("dialogue.txt");
-        reader = theSourceFile.OpenText();
+        dialogueText = new FileInfo("Assets/Scripts/dialogue.txt");
+        reader = dialogueText.OpenText();
     }
 
     // Update is called once per frame
     void Update() {
         if((line = reader.ReadLine()) != null) 
         {
-            text = reader.ReadLine();
-            //Console.WriteLine(text);
-            print (text);
+			string[] output = line.Split('(', ')');
+			Debug.Log(output);
+
         }        
     }
 }
